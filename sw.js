922c6e68a27ecc08f99ed814dd82d7fc7f5fcda4
Julien Cheype <cheype@gmail.com>
2016-10-05 20:35:21 +0200

    test

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 (global => {
   'use strict';
 
+var urls = [
+  '/companion.js',
+  '/polyfills.bundle.js',
+  '/vendor.bundle.js',
+  '/main.bundle.js',
+  '/sw-toolbox.js',
+  '/sw.js',
+];
+
   // Load the sw-toolbox library.
   importScripts('/sw-toolbox.js');
   
@@ -8,7 +17,8 @@
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
   global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
 
-
   // The route for any requests from the googleapis origin
-  toolbox.router.get('/(.*)', global.toolbox.cacheFirst);
+  toolbox.router.get('/(.*)', global.toolbox.networkFirst);
+  toolbox.precache(urls);
+
 })(self);
